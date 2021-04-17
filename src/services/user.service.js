7f859aff8eedcd728c90dbdf7a8e14a0d7cdee38
BeaTea109 <beatea109@hotmail.com>
2021-04-17 18:36:33 -0500

    Updated database URI

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:5050/api/';
+const API_URL = 'https://info3180-project-2-backend.herokuapp.com/api/';
 
 class UserService {
 
